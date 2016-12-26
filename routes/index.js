df2a90ac48f118b1a21d879034797bd4c6a1c806
Ali Mousavi <ali.mousavi@gmail.com>
2016-12-26 12:43:44 +0330

    Added upload form and POST functionality

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 /* jshint esversion: 6 */
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ storage: multer.memoryStorage(),
+                      limits: {
+                          fileSize: 50000000
+                      }
+                    });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Tuxitop File Size API',
                           baseURL: req.get('host')});
+});
+
+/* POST the file */
+router.post('/', upload.single("file"), (req, res, next) => {
+    res.send({
+        size: req.file.size
+    });
 });
 
 module.exports = router;
